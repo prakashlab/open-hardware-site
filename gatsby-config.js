072f3b671d209be255f438a4bbceed8cd1bbac98
Ethan Li <lietk12@gmail.com>
2020-07-03 12:03:10 -0700

    Updated Gatsby versions for bugfixes for building and deployment.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: "Open Hardware Site",
     description: "Starter for open hardware project sites.",
-    siteUrl: process.env.SITE_URL || "http://localhost:8000",
+    siteUrl: process.env.SITE_URL || "https://ethanjli.github.io",
   },
   pathPrefix: "/open-hardware-site",
   plugins: [
